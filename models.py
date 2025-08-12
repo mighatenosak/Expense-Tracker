98b7b3703cb13fb51aa9ac5a5efff6e60531db57
mighatenosak <khanalisak1113@gmail.com>
2025-08-12 21:38:35 +0530

    Added RegisterUser Model

--- models.py
+++ models.py
@@ -1,9 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 from datetime import date
 
+#for expenses
 class Expense(BaseModel):
     amount: float
     category: str
     date: date
     description: Optional[str] = None
+#For the registration of users
+class RegisterUser(BaseModel):
+    full_name: str
+    email: EmailStr
+    password: str
