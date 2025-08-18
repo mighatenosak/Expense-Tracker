f425efa16773538a12a3342774634d19e613cb9b
mighatenosak <khanalisak1113@gmail.com>
2025-08-18 15:38:26 +0530

    New Features for Admin

--- crud.py
+++ crud.py
@@ -1,98 +1,109 @@
 #Connect to MongoDB
 from data_base import expenses_collection, categories_collection, users_collection, roles_collection
-#For schema
+#imports the Pydantic model for validating expense data.
 from models import Expense  
 from datetime import datetime, date
+#CryptoContext for hashing and verifying passwords
 from passlib.context import CryptContext
-from typing import Union, Optional
+from typing import Union, Optional,Any  #'Any'type-hint that a variable can hold any data
+#bson.objectid to convert str into objectid
 from bson import ObjectId
-from typing import Any
 
-#Create expense
+
+#create expense
 def add_expense(expense: Union[Expense, dict], current_user: dict):
-    """Add a new expense linked to a specific user."""
-    # Convert Pydantic model to dict if needed
+    #convert Pydantic model to dict if needed
+    #if it’s a Pydantic object convert to dictionary using .model_dump().
     if isinstance(expense, Expense):
         data = expense.model_dump()
-    else:
+    else:   #if it’s already a dictionary, make a copy.
         data = dict(expense)
 
-    # Attach the user_id
+    # Attach the user_id to the expense(converted to str because Mongo IDs are objs)
     data["user_id"] = str(current_user["_id"])
 
-    # Ensure date is ISO string
+    # Ensure date is ISO string in format (YYYY-MM-DD)which is MongoDB-friendly
     if isinstance(data.get("date"), (datetime, date)):
         data["date"] = data["date"].isoformat()
 
-    return expenses_collection.insert_one(data)
+    return expenses_collection.insert_one(data) #return expense
 # def add_expense(expense: Expense):
     # return expenses_collection.insert_one(expense.dict())
     # return expenses_collection.insert_one(expense.model_dump())
 
-#Get all expenses
+#get all expenses
 def get_all_expenses(user_id: str):
-    expenses = list(expenses_collection.find({"user_id": user_id}))
+    expenses = list(expenses_collection.find({"user_id": user_id})) #fetch expense
+    #convert Mongo ID obj to str for json
     for exp in expenses:
         exp["_id"] = str(exp["_id"])
-    return expenses
-    
-    return expenses
+    return expenses #return list of expenses
+
 #create category
 def add_category(name: str):
-    #Prevent duplicates
+    #prevents duplicate categories
     if categories_collection.find_one({"name": name}):
         return {"msg": "Category already exists"}
-    categories_collection.insert_one({"name": name})
+    categories_collection.insert_one({"name": name})    #insert category into DB
     return {"msg": "Category added"}
 
-#Get All Category
+#get all category
 def get_categories():
+    #retrieve all categories and covert obj id to str
     categories = list(categories_collection.find())
     for cat in categories:
-        cat["_id"] = str(cat["_id"])  #Convert ObjectId to string
+        cat["_id"] = str(cat["_id"])  #convert ObjectId to string
     return categories
 
 
-#Filter by date
+#filter by date
 # def get_expenses_by_date_range(start, end):
 def get_expenses_by_date_range(start, end, user_id: str):
+    #convert python date objects to str
     start_str = start.isoformat()
     end_str = end.isoformat()
+    #filter expenses between start and end dates for the given user
     expenses = list(expenses_collection.find({
         "user_id": user_id,
         "date": {"$gte": start_str, "$lte": end_str}
     }))
+    #convert obj id to str
     for exp in expenses:
         exp["_id"] = str(exp["_id"])
     return expenses
 
 
 
-#Filter by category
+#filter by category
 def get_expenses_by_category(category, user_id: str):
+    #find expenses for a user where the category matches
     expenses = list(expenses_collection.find({
         "user_id": user_id,
         "category": category
     }))
+    #convert obj id to str
     for exp in expenses:
         exp["_id"] = str(exp["_id"])
     return expenses
-#Monthly total summary
+#monthly total summary
 def get_total_for_month(month: str, user_id: str):
+    #here i used regex to match all dates starting with "YYYY-MM"
     expenses = list(expenses_collection.find(
         {
             "user_id": user_id,
             "date": {"$regex": f"^{month}"}
         }
     ))
+    #sums total amount for that month
     total = sum(exp["amount"] for exp in expenses)
     return {"month": month, "total": total}
 
 
 
-#Top3 spending categories where the most money was spent
+#top3 spending categories where the most money was spent
 def get_top_categories(user_id: str, limit: int = 3):
-    # Filter first by user_id
+    #using MONGODB's aggregation pipeline to filter by user, group by category and sum amounts, sort by total spent
+    #and limit to top n categories(default 3)
     pipeline = [
         {"$match": {"user_id": user_id}},
         {"$group": {
@@ -121,13 +132,14 @@ def register_user(full_name: str, email: str, password: str):
     users_collection.insert_one({
         "full_name": full_name,
         "email": email,
-        "password_hash": hash_password(password),
+        "password_hash": hash_password(password),   #password hashing using bcrypt algo
         "role_id": role_id
     })
     return {"msg": "User registered successfully"}
 
 #login user
 def login_user(email: str, password: str):
+    #verify email and password by matching
     user = users_collection.find_one({"email": email})
     if not user:
         return None
@@ -135,28 +147,31 @@ def login_user(email: str, password: str):
         return None
     return user
 
-#add password hashing utility using bcrypt from Cryptocontext
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+#add password hashing utility using bcrypt algorithm from Cryptocontext
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")   #'deprecated=auto' automatically rehashes and old hashing scheme if detected.
+#hashing pass
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
-# pass verification
+#pass verification
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 #get roll name
 
 def get_role_name(role_id: Union[str, ObjectId]) -> Optional[str]:
-    """Return the role name for a given role_id, or None if not found."""
+    #convert string ids to obj id
     if isinstance(role_id, str):
         try:
             role_id = ObjectId(role_id)
         except Exception:
             return None
+    #fetch the role name
     role = roles_collection.find_one({"_id": role_id})
     return role["name"] if role else None
 
 #view users(admin only)
 def get_all_users():
+    #retrieve all users excluding pass hashes
     users = list(users_collection.find({}, {"password_hash": 0}))  # exclude passwords
     for user in users:
         user["_id"] = str(user["_id"])
@@ -164,26 +179,29 @@ def get_all_users():
     return users
 
 #update expenses (for users)
+#admins can edit any expense, normal users can only edit their own expenses
 def update_expense(expense_id: str, updates: dict, current_user: dict, admin: bool = False):
     query: dict[str, Any] = {"_id": ObjectId(expense_id)}
     if not admin:
         query["user_id"] = str(current_user["_id"])  # match stored string user_id
 
-    # Clean empty fields
+    #remove empty update fields before updating
     updates = {k: v for k, v in updates.items() if v is not None and v != ""}
-
     result = expenses_collection.update_one(query, {"$set": updates})
+    
     if result.modified_count == 0:
         return {"error": "No expense updated. Check ID or permissions."}
     return {"msg": "Expense updated successfully"}
 
 #delete user(admin)
 def delete_user(user_id: str):
+    #delete user by id
     result = users_collection.delete_one({"_id": ObjectId(user_id)})
     return {"deleted_count": result.deleted_count}
 
 #update user(admin)
 def update_user(user_id: str, updates: dict):
+    #if pass is updated, hash it before storing
     if "password" in updates:
         updates["password_hash"] = hash_password(updates.pop("password"))
     result = users_collection.update_one({"_id": ObjectId(user_id)}, {"$set": updates})
